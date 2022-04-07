4976fea18d38fb4bf88485368e10906d1b9ecb6f
alvarengacarlos <carlosho200@hotmail.com>
2022-04-06 22:15:11 -0400

    Cria view para leitura das farmacias

--- encontraFarma/views.py
+++ encontraFarma/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .models import Farmacia
 from .serializer import FarmaciaSerializer
 
-class FarmaciasViewSet(viewsets.ModelViewSet):
+class FarmaciasViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Farmacia.objects.all()
     serializer_class = FarmaciaSerializer
 
