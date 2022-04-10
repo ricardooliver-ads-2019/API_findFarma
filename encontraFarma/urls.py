038399dfa617bc27d14d9950445445be29a0a420
alvarengacarlos <carlosho200@hotmail.com>
2022-04-10 18:41:55 -0400

    Cria nova rota para farmacias plantonistas

--- encontraFarma/urls.py
+++ encontraFarma/urls.py
@@ -1,6 +1,7 @@
-from encontraFarma.views import FarmaciasViewSet, FarmaciasAbertasViewSet
+from encontraFarma.views import FarmaciasViewSet, FarmaciasAbertasViewSet, FarmaciasPlantaoHojeViewSet
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register('farmacias', FarmaciasViewSet)
 router.register('farmacias-abertas', FarmaciasAbertasViewSet)
+router.register('farmacias-plantao-hoje', FarmaciasPlantaoHojeViewSet)
